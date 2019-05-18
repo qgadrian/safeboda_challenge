a32ddbc1d3bd2ca8e188a563fddd9b957c1232a4
qgadrian <qgadrian@users.noreply.github.com>
2019-05-18 20:19:11 +0200

    chore: add frequency to active field in generator

--- apps/promo_code_generator/lib/safe_boda/promo_code/promo_code_generator.exs
+++ apps/promo_code_generator/lib/safe_boda/promo_code/promo_code_generator.exs
@@ -15,16 +15,20 @@ defmodule SafeBoda.PromoCode.Generator.PromoCode do
 
   ## Options:
 
+  * `frequency_active`: Then frequency of when the promo code is active.
+  * `frequency_inactive`: Then frequency of when the promo code is inactive.
   * `max_number_of_rides`: The maximum number of rides for the promo code, defaults to `999999`.
   * `minimum_event_radius`: The minimum radius from the promo code, defaults to `10000`.
   * `valid_code?`: Whether the code generated should be valid or not, defaults to `true`.
   """
   def generate_promo_code(opts \\ []) do
+    frequency_active = Keyword.get(opts, :frequency_active, 1)
+    frequency_inactive = Keyword.get(opts, :frequency_inactive, 1)
     max_number_of_rides = Keyword.get(opts, :max_number_of_rides, 999_999)
     minimum_event_radius = Keyword.get(opts, :minimum_event_radius, 999_999)
     valid_code? = Keyword.get(opts, :valid_code?, true)
 
-    gen all active? <- StreamData.boolean(),
+    gen all active? <- generate_active?(frequency_active, frequency_inactive),
             code <- generate_code(valid_code?),
             description <- StreamData.string(:alphanumeric, min_length: 1),
             expiration_date <- DateTimeGenerator.gen_datetime(),
@@ -55,6 +59,14 @@ defmodule SafeBoda.PromoCode.Generator.PromoCode do
       expiration_date: DateTime.utc_now(),
       code: "PROMOCODE"
     }
+  end
+
+  @spec generate_active?(pos_integer, pos_integer) :: StreamData.t(binary)
+  defp generate_active?(freq_active, freq_inactive) do
+    StreamData.frequency([
+      {freq_active, StreamData.member_of([true])},
+      {freq_inactive, StreamData.member_of([false])}
+    ])
   end
 
   @spec generate_code(boolean) :: StreamData.t(binary)
