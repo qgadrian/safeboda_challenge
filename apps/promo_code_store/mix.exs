adc3243bf308acec35ff6049cb558c9155d63ead
qgadrian <qgadrian@users.noreply.github.com>
2019-05-19 20:40:58 +0200

    chore: only use generators on test profile

--- apps/promo_code_store/mix.exs
+++ apps/promo_code_store/mix.exs
@@ -40,7 +40,7 @@ defmodule SafeBoda.PromoCodeStore.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:geocalc, "~> 0.5"},
       {:polyline, "~> 1.2"},
-      {:promo_code_generator, in_umbrella: true}
+      {:promo_code_generator, in_umbrella: true, only: :test}
     ]
   end
 end
