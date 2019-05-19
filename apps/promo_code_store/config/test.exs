6cda99e44f0e780cdad2e1b9346e4ad67af793e6
qgadrian <qgadrian@users.noreply.github.com>
2019-05-19 20:18:06 +0200

    chore: use own database in tests

--- apps/promo_code_store/config/test.exs
+++ apps/promo_code_store/config/test.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
-config :promo_code_store, SafeBoda.PromoCodeStore.Repo, pool: Ecto.Adapters.SQL.Sandbox
+config :promo_code_store, SafeBoda.PromoCodeStore.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: "safeboda_test"
