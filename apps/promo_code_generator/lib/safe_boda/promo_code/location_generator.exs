5b4582b41e7d67d72db7a35a8048a452f5ad0582
qgadrian <qgadrian@users.noreply.github.com>
2019-05-19 14:39:25 +0200

    refactor: max and min longitude in generator

--- apps/promo_code_generator/lib/safe_boda/promo_code/location_generator.exs
+++ apps/promo_code_generator/lib/safe_boda/promo_code/location_generator.exs
@@ -8,10 +8,23 @@ defmodule SafeBoda.PromoCode.Generator.Location do
 
   @doc """
   Returns a generator for `t:#{Location}.t/0`.
+
+  ## Options
+
+  * `max_latitude`: The maximum latitude point.
+  * `max_longitude`: The maximum longitude point.
+  * `min_latitude`: The minimum latitude point.
+  * `min_longitude`: The minimum longitude point.
   """
-  def generate_location() do
-    gen all latitude <- StreamData.float(),
-            longitude <- StreamData.float() do
+  @spec generate_location(keyword) :: StreamData.t(Location.t())
+  def generate_location(opts \\ []) do
+    max_latitude = Keyword.get(opts, :max_latitude, 90)
+    max_longitude = Keyword.get(opts, :max_longitude, 180)
+    min_latitude = Keyword.get(opts, :min_latitude, -90)
+    min_longitude = Keyword.get(opts, :min_longitude, -180)
+
+    gen all latitude <- StreamData.float(min: min_latitude, max: max_latitude),
+            longitude <- StreamData.float(min: min_longitude, max: max_longitude) do
       %Location{latitude: latitude, longitude: longitude}
     end
   end
